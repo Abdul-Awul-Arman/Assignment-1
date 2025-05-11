92cfbe43484d8570146fc9b0378949ba3126cbbb
Abdul-Awul-Arman <abdulawularman52114@gmail.com>
2025-05-11 11:58:00 +0600

    finished 5th problem

--- solution.ts
+++ solution.ts
@@ -1,1 +1,66 @@
-console.log("arman");
+function formatString(input: string, toUpper?: boolean): string{
+  
+
+    if(toUpper=== true || toUpper== undefined){
+       return input.toUpperCase()
+    }
+
+    return input.toLowerCase();
+};
+
+
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+
+  type RatingType={
+    title:string;
+    rating:number
+  };
+
+   let filtered:RatingType[]=items.filter((item:RatingType)=>item.rating>=4)
+
+  return filtered;
+
+};
+
+function concatenateArrays<T>(...arrays: T[][]): T[]{
+
+    let result:Array<T>=[];
+
+   return result.concat(...arrays);
+
+};
+
+class Vehicle{
+    private _make:string;
+     year:number;
+
+     constructor(make:string,year:number){
+        this._make=make;
+        this.year=year;
+     };
+
+     getInfo(){
+        console.log(`Make: ${this._make}, Year: ${this.year}`)
+     };
+};
+
+
+
+class Car extends Vehicle{
+    private _model:string;
+
+   constructor(make:string,year:number,model:string){
+       super(make,year);
+       this._model=model;
+   };
+
+   getModel(){
+    console.log(`Model: ${this._model}`)
+   }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo();   // Output: "Make: Toyota, Year: 2020"
+myCar.getModel();  // Output: "Model: Corolla"
+
